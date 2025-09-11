c1c46345026c51f5549866e26f44bc08d56539a1
Abinashpatri07 <abinashpatri5@gmail.com>
2025-09-11 21:12:49 +0530

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://anjali-infra.vercel.app', // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
